0cb4bbef08f34a05640a7ef64b74d58c73c8cad0
Nathan <nateinthegame@gmail.com>
2019-02-16 14:02:28 -0800

    u

--- TopKFrequentWords.js
+++ TopKFrequentWords.js
@@ -1,8 +1,11 @@
 const topKFrequent = (words, k) => {
-    let map = new Map()
-    words.map(word => {
-        map.has(word) ? map.set(word, map.get(word)+1) : map.set(word, 1)
-    })
-    let sortedMap = new Map([...map.entries()].sort().sort((a, b) => b[1] - a[1]))
-    return [...sortedMap.keys()].slice(0, k)
-}
+  let map = new Map();
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    map.has(word) ? map.set(word, map.get(word) + 1) : map.set(word, 1);
+  }
+  let sortedMap = new Map(
+    [...map.entries()].sort().sort((a, b) => b[1] - a[1])
+  );
+  return [...sortedMap.keys()].slice(0, k);
+};
